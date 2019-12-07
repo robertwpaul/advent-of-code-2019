bf3cbecb1e4cad375d4751862b451170e339a2c9
Robert Paul <hello@robertpaul.io>
2019-12-07 21:22:37 +0000

    day 4, part 1 solution

---
+++ day-04/solution-part-1.rb
@@ -0,0 +1,23 @@
+range = 153517..630395
+
+def adjacent_digits(num)
+  num.match?(/([0-9])\1/)
+end
+
+def no_decreasing_digits(num)
+  result = true
+  num.chars.each_cons(2) do |a, b|
+    result = false if b < a
+  end
+
+  result
+end
+
+suitable = []
+
+range.each do |num|
+  num_str = num.to_s
+  suitable << num if adjacent_digits(num_str) && no_decreasing_digits(num_str)
+end
+
+puts suitable.length
